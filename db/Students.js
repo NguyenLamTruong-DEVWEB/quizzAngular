bad6124dfe9ba95f0db5641166983d20bbff3b3a
NguyenLamTruong-DEVWEB <93717331+NguyenLamTruong-DEVWEB@users.noreply.github.com>
2022-02-20 08:00:04 +0700

    upload

---
+++ db/Students.js
@@ -0,0 +1,29 @@
+﻿var listsv = [
+    {
+        "username": "truongdouble",
+        "password": "anhyeuem",
+        "fullname": "Nguyễn Lam Trường",
+        "email": "truongnlps13240@fpt.edu.vn",
+        "birthday": "1995-12-21",
+        "schoolfee": "1500000",
+        "marks": "8"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "9"
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "5"
+    }
+]
---
+++ db/Subjects.js
@@ -0,0 +1,123 @@
+[
+    {
+        "Id": "ADAV",
+        "Name": "Lập trình Android nâng cao",
+        "Logo": "ADAV.jpg",
+        "icon" : "Androind Cơ Bản"
+    },
+    {
+        "Id": "ADBS",
+        "Name": "Lập trình Android cơ bản",
+        "Logo": "ADBS.jpg",
+        "icon" : "Androind Nâng Cao"
+    },
+    {
+        "Id": "ADTE",
+        "Name": "Kiểm thử ứng dụng Android",
+        "Logo": "ADTE.jpg",
+        "icon" : "App Androind"
+    },
+    {
+        "Id": "ADUI",
+        "Name": "Thiết kế trên Android",
+        "Logo": "ADUI.jpg",
+        "icon" : "UI Androind"
+    },
+    {
+        "Id": "ASNE",
+        "Name": "Lập trình ASP.NET",
+        "Logo": "ASNE.png",
+        "icon" : "ASP.NET"
+    },
+    {
+        "Id": "CLCO",
+        "Name": "Điện toán đám mây",
+        "Logo": "CLCO.jpg",
+        "icon" : "CLOUD ELECTRIC"
+    },
+    {
+        "Id": "DBAV",
+        "Name": "SQL Server",
+        "Logo": "DBAV.png",
+        "icon" : "SQL SERVER"
+    },
+    {
+        "Id": "DBBS",
+        "Name": "Cơ sở dữ liệu",
+        "Logo": "DBBS.png",
+        "icon" : "DATABASE"
+    },
+    {
+        "Id": "GAME",
+        "Name": "Lập trình game 2D",
+        "Logo": "GAME.png",
+        "icon" : "GAME "
+    },
+    {
+        "Id": "HTCS",
+        "Name": "HTML5 và CSS3",
+        "Logo": "HTCS.jpg",
+        "icon" : "HTML5 AND CSS3"
+    },
+    {
+        "Id": "INMA",
+        "Name": "Internet Marketing",
+        "Logo": "INMA.jpg",
+        "icon" : "MARKETING"
+
+    },
+    {
+        "Id": "JAAV",
+        "Name": "Lập trình Java nâng cao",
+        "Logo": "JAAV.png",
+        "icon" : "JAVA"
+    },
+    {
+        "Id": "JABS",
+        "Name": "Lập trình đối tượng Java",
+        "Logo": "JABS.png",
+        "icon" : "JAVA OOP"
+    },
+    {
+        "Id": "JSPR",
+        "Name": "Lập trình JavaScript",
+        "Logo": "JSPR.png",
+        "icon" : "JAVASCRIPT"
+    },
+    {
+        "Id": "LAYO",
+        "Name": "Thiết kế layout",
+        "Logo": "LAYO.jpg",
+        "icon" : "UX/UX"
+    },
+    {
+        "Id": "MOWE",
+        "Name": "Thiết điện thoại di động",
+        "Logo": "MOWE.png",
+        "icon" : "MOBILE APP"
+    },
+    {
+        "Id": "PHPP",
+        "Name": "Lập trình PHP",
+        "Logo": "PHPP.png",
+        "icon" : "PHP"
+    },
+    {
+        "Id": "PMAG",
+        "Name": "Quản lý dự án với Agile",
+        "Logo": "PMAG.jpg",
+        "icon" : "AGILE"
+    },
+    {
+        "Id": "VBPR",
+        "Name": "Lập trình VB.NET",
+        "Logo": "VBPR.png",
+        "icon" : "VB.NET"
+    },
+    {
+        "Id": "WEBU",
+        "Name": "Xây dựng trang web",
+        "Logo": "WEBU.jpg",
+        "icon" : "BUILD LAYOUT"
+    }
+]
